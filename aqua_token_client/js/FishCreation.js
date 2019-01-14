2ea2f29b503b19a3bf26fc66d22d163d0f28f36e
LasterSlaster <lasterslaster@gmx.de>
2019-01-14 18:33:01 +0100

    formatting

--- aqua_token_client/js/FishCreation.js
+++ aqua_token_client/js/FishCreation.js
@@ -18,7 +18,6 @@ async function createFish() {
    }).catch(function(error){
     console.log(error)
    });
-
  */
   return Promise.resolve([contractResult]);
 }
@@ -32,49 +31,48 @@ function insertFish(fishToken) {
 
 function insertRandomFish(fish) {
   //AquaPresentation: inserts random fish into svg div "aquarium":
- fish.posX =Math.round(Math.random() * 300) + 200;
- fish.posY = Math.round(Math.random() * 200);
-insertFishToAquarium(fish);
+  fish.posX =Math.round(Math.random() * 300) + 200;
+  fish.posY = Math.round(Math.random() * 200);
+  insertFishToAquarium(fish);
 
   return fish;
 }
 
-function readAllFishes(){
-  aquaTokenContract.allOwnedTokens().then(function(result){
-   
-    if(result.length ==0 ){
+
+function readAllFishes() {
+  aquaTokenContract.allOwnedTokens().then(function(result) {
+    if(result.length == 0) {
       return;
     }
-    var token_ids_String="";
+
+    var token_ids_String = "";
     for(i in result){
-    token_ids_String+= result[i] +",";
-  }
+      token_ids_String += result[i] + ",";
+    }
 
-  token_ids_String = token_ids_String.substr(0, token_ids_String.length-1);
-  fishTokenDatabase.getFishTokensByIds(token_ids_String).then(function(result){
-  for( element in result){
-    var fish= result[element];
-    insertFish(fish);
-  }
+    token_ids_String = token_ids_String.substr(0, token_ids_String.length-1);
 
+    fishTokenDatabase.getFishTokensByIds(token_ids_String).then(function(result) {
+      for(element in result) {
+        var fish = result[element];
+        insertFish(fish);
+      }
+    });
   });
-});
-
-
 }
 
- async function pairFishes(){
+
+async function pairFishes() {
   //console.log(selectedFish,fishArray[fishCount]);
   console.log("test");
- var contractResult = await aquaTokenContract.mathFish(selectedFish, fishArray[fishCount]);
+  var contractResult = await aquaTokenContract.mathFish(selectedFish, fishArray[fishCount]);
   //convert Speed back to floatingPoint 
-  var convertSpeed = Number.parseFloat(contractResult.speed)/1000;
+  var convertSpeed = Number.parseFloat(contractResult.speed) / 1000;
   var name = nameList[Math.floor(Math.random() * 9505) + 1];
-  var fishToken = new FishToken(contractResult.id,name, convertSpeed,contractResult.kopf, contractResult.schwanz );
+  var fishToken = new FishToken(contractResult.id,name, convertSpeed,contractResult.kopf, contractResult.schwanz);
 
-   await fishTokenDatabase.createOrUpdateFishToken(fishToken).then(result =>{
+  await fishTokenDatabase.createOrUpdateFishToken(fishToken).then(result => {
     insertFish(fishToken);
     console.log("create fish by name: " + fishToken.name);
-   });
-
+  });
 }
